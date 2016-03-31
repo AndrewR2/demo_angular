b594e0c25ef9295aecc329a5ef0bb57a72dc66cb
Andres Rocafull <arocafull@gmail.com>
2016-03-31 18:01:14 +0200

    Pruebas curso CodeSchool

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,33 @@
 'use strict';
 
 (function() {
-  console.log('Hola mundo');
+var app = angular.module('demo', []);
+
+var gemas = [
+  {
+    nombre: 'Diamante',
+    precio: 5,
+  },
+  {
+    nombre: 'Gema',
+    precio: 4,
+  }
+];
+
+app.controller('ProductosCtrl', function(){
+  this.productos = gemas;
+});
+
+app.controller('PanelCtrl', function() {
+  this.tab = 1;
+
+  this.selectTab = function(setTab) {
+    this.tab = setTab;
+  };
+
+  this.isTab = function(checkTab) {
+    return this.tab === checkTab;
+  };
+});
+
 })();
