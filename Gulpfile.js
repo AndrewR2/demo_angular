3f9e3a5aff97c348b1d728cd9fdc78bb5184779d
Andres Rocafull <arocafull@gmail.com>
2016-03-31 16:50:43 +0200

    Primera versión que funciona

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,22 +2,16 @@
 
 var gulp = require('gulp'),
 connect = require('gulp-connect'),
-historyApiFallback = require('connect-history-api-fallback'),
 stylus = require('gulp-stylus'),
-nib = require('nib'),
-jshint = require('gulp-jshint'),
-stylish = require('jshint-stylish');
+jshint = require('gulp-jshint');
 
 // Servidor web de desarrollo
 gulp.task('server', function() {
   connect.server({
-    root: './app',
-    host: '0.0.0.0',
+    root: 'app',
+    hostname: '0.0.0.0',
     port: 8080,
-    livereload: true,
-    middleware: function(connect, opt) {
-      return [ historyApiFallback ];
-    }
+    livereload: true
   });
 });
 
@@ -46,35 +40,12 @@ gulp.task('html', function() {
   .pipe(connect.reload());
 });
 
-var inject = require('gulp-inject');
-var wiredep = require('wiredep').stream;
-// Busca en las carpetas de estilos y javascript los archivos que hayamos creado
-// para inyectarlos en el index.html
-gulp.task('inject', function() {
-var sources = gulp.src(['./app/scripts/**/*.js','./app/stylesheets/**/*.css']);
-return gulp.src('index.html', {cwd: './app'})
-.pipe(inject(sources, {
-read: false,
-ignorePath: '/app'
-}))
-.pipe(gulp.dest('./app'));
-});
-// Inyecta las librerias que instalemos vía Bower
-gulp.task('wiredep', function () {
-gulp.src('./app/index.html')
-.pipe(wiredep({
-directory: './app/lib'
-}))
-.pipe(gulp.dest('./app'));
-});
-
 // Vigila cambios que se produzcan en el código
 // y lanza las tareas relacionadas
 gulp.task('watch', function() {
   gulp.watch(['./app/**/*.html'], ['html']);
   gulp.watch(['./app/stylesheets/**/*.styl'], ['css']);
   gulp.watch(['./app/scripts/**/*.js', './Gulpfile.js'], ['jshint']);
-  gulp.watch(['./bower.json'], ['wiredep']);
 });
 
-gulp.task('default', ['server', 'inject', 'wiredep', 'watch']);
+gulp.task('default', ['server', 'watch']);
